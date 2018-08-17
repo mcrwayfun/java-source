c9ec6ee2a3865c1ad12134d7bdf6e6f840d6966d
mcrwayfun <mcrwayfun@gamil.com>
2018-08-17 14:56:46 +0800

    更新

---
+++ project/cookie-session-demo/src/main/resources/static/js/CookieUtil.js
@@ -0,0 +1,32 @@
+//设置cookie，使之能跨域获取
+function setCookie(name, value) {
+    var Days = 30;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+    document.cookie = name + "=" + value + ";expires=" + exp.toGMTString() + "; path=/" + ";domain=.mcrwayfun.com";
+}
+
+//读取cookies
+function getCookie(name) {
+    var arr,
+        reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+    if (arr = document.cookie.match(reg)) {
+        return arr[2];
+    }
+    else {
+        return null;
+    }
+}
+
+//删除cookies
+function delCookie(name) {
+    var exp = new Date();
+    exp.setTime(exp.getTime() - 1);
+    var cval = getCookie(name);
+    if (cval != null)
+        document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+}
+
+//使用举例
+setCookie("mcrwayfun","test cookie value");
+getCookie("mcrwayfun");
